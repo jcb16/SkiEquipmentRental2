4f7f08f81f5206352803e43ab427499265906018
Jakub Kolber <115322984+jcb16@users.noreply.github.com>
2023-06-09 23:10:43 +0200

    Modify DataRow in Employees

--- ViewModel/Employee.xaml.cs
+++ ViewModel/Employee.xaml.cs
@@ -20,13 +20,9 @@ namespace SkiEquipmentRental2.ViewModel
 
     public partial class Employee
     {
-        //public string FirstName { get; set; }
-        //public string LastName { get; set; }
-        //public string Data_ur { get; set; }
-        //public string Pesel { get; set; }
-        //public string Numer_tel { get;set; }
-        //public string Login { get; set; }
-        //public string Password { get; set; }
+        SqlDataAdapter sda;
+        SqlCommandBuilder scb;
+        DataTable dt;
 
         private void selectEmployee_Click(object sender, RoutedEventArgs e)
         {
@@ -64,6 +60,58 @@ namespace SkiEquipmentRental2.ViewModel
                     dataGrid.ItemsSource = dtbl.DefaultView;
                 }
             }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //SqlConnection con = new SqlConnection("Data Source=DESKTOP-RVQS4VV;Initial Catalog=WypozyczalniaFull;Integrated Security=True");
+            //sda = new SqlDataAdapter(@"SELECT * FROM tblPracownicy",con);
+            //dt = new DataTable();
+            //sda.Fill(dt);
+            //scb = new SqlCommandBuilder(sda);
+            //sda.Update(dt);
+
+            string connstring = "Data Source=DESKTOP-RVQS4VV;Initial Catalog=WypozyczalniaFull;Integrated Security=True";
+            string updateQuery = "UPDATE tblPracownicy SET Imie = @Imie, Nazwisko = @Nazwisko, Data_ur = @Data_ur, PESEL = @PESEL, Numer_tel = @Numer_tel,Login_prac = @Login_prac,  Password_prac=@Password_prac WHERE IDPracownik = @IDPracownik";
+
+            using (SqlConnection con = new SqlConnection(connstring))
+            {
+                con.Open();
+
+                using (SqlCommand updateCmd = new SqlCommand(updateQuery, con))
+                {
+                    foreach (DataRowView rowView in dataGrid.ItemsSource)
+                    {
+                        DataRow row = rowView.Row;
+                        //updateCmd.Parameters.Clear();
+                        //updateCmd.Parameters.AddWithValue("@Imie", row["Imie"]);
+                        //updateCmd.Parameters.AddWithValue("@Nazwisko", row["Nazwisko"]);
+                        //updateCmd.Parameters.AddWithValue("@IDPracownik", row["IDPracownik"]);
+                        updateCmd.Parameters.Clear();
+                        updateCmd.Parameters.AddWithValue("@Imie", row["Imie"].ToString());
+                        updateCmd.Parameters.AddWithValue("@Nazwisko", row["Nazwisko"].ToString());
+                        updateCmd.Parameters.AddWithValue("@Data_ur", Convert.ToDateTime(row["Data_ur"]));
+                        updateCmd.Parameters.AddWithValue("@PESEL", row["PESEL"].ToString());
+                        updateCmd.Parameters.AddWithValue("@Numer_tel", row["Numer_tel"].ToString());
+                        updateCmd.Parameters.AddWithValue("@Login_prac", row["Login_prac"].ToString());
+                        updateCmd.Parameters.AddWithValue("@Password_prac", row["Password_prac"].ToString());
+                        updateCmd.Parameters.AddWithValue("@IDPracownik", Convert.ToInt32(row["IDPracownik"]));
+
+
+
+                        //updateCmd.Parameters.AddWithValue("@Pesel", row["Pesel"]);
+                        //updateCmd.Parameters.AddWithValue("@Numer_tel", row["Numer telefonu"]);
+                        //updateCmd.Parameters.AddWithValue("@Login", row["Login"]);
+                        //updateCmd.Parameters.AddWithValue("@Password", row["Hasło"]);
+
+
+
+                        updateCmd.ExecuteNonQuery();
+                    }
+                }
+            }
+
+
         }
     }
 
